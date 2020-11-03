cdcc2a2c13ca5fa6179adabe96175b1e8f74c5cf
shane-decatur <sdecatur20@stuy.edu>
2020-11-02 20:10:50 -0500

    added set()

--- SuperArray.java
+++ SuperArray.java
@@ -20,4 +20,9 @@ public class SuperArray{
   public String get(int index){
     return data[index];
   }
+
+  public String set(int index, String element){
+    data[index] = element;
+    return data[index];
+  }
 }
