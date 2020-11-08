de49494dd456e717e063b91591feee8846412d26
shane-decatur <sdecatur20@stuy.edu>
2020-11-08 13:58:00 -0500

    added findOverlap()

--- Demo.java
+++ Demo.java
@@ -8,6 +8,18 @@ public class Demo{
       }
     }
   }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    SuperArray c = new SuperArray();
+    for (int i = 0; i < a.size(); i++){
+      for (int j = 0; j < b.size(); j++){
+        if (a.get(i) == b.get(j)) c.add(a.get(i));
+      }
+    }
+    removeDuplicates(c);
+    return c;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -15,7 +27,15 @@ public class Demo{
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
 
+    SuperArray nums1 = new SuperArray();
+    SuperArray nums2 = new SuperArray();
+
+    nums1.add("9"); nums1.add("1"); nums1.add("2"); nums1.add("2"); nums1.add("3"); nums1.add("4");
+    nums2.add("0"); nums2.add("4"); nums2.add("2"); nums2.add("2"); nums2.add("9");
+
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+    System.out.println(findOverlap(nums1, nums2));
+  }
 }
